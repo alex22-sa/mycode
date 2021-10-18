6da169cba8d3eec1cacf3793b5bbb81c99aab2f2
Alex Latovin <al22@ntrs.com>
2021-10-18 16:52:41 +0000

    learning about input function

---
+++ lab_input/iptaker03.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE 01 - Solution"""    
+
+def main():
+
+    user_input = input("What is your name: ")
+    
+    print("Hello:", user_input)
+    
+    # asking user for 'vendor name'
+    date = input("What is the date today: ")
+
+
+main()
+
+
